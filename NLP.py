efd9c227e37e1f1aff441b4c4fecfa24e0d32818
Kimberly Schlesinger <kschlesi42@gmail.com>
2017-07-30 00:35:27 -0700

    developing analysis code

--- NLP.py
+++ NLP.py
@@ -1,19 +1,26 @@
 '''this is a file of NLP processing and modelling classes'''
 
 import pandas as pd
+import string
+import re
+
 from nltk import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
-import string
-import re
+
+from gensim import corpora, models, similarities
+from collections import defaultdict
+
 
 class NLProcessor():
     '''Class with methods to get, set, store, and basically process text data'''
 
     def __init__(self, raw_text=[], swords=None):
         self.raw_text = raw_text
-        self.processed_text = []
-        self.removed_articles = []
+        self.processed_text = None
+        self.flat_text = None
+        self.removed_articles = None
+        self.word_count = None
         if swords == None:
             self.swords = stopwords.words('english')
         else:
@@ -49,7 +56,7 @@ class NLProcessor():
             return df
 
     def process_text(self, in_text=None, break_on=['.','?','!'], stopwords='default', to_stem=True, 
-                        init_split_on='database', origdb=None):
+                        init_split_on='database', origdb=None, ddiv_max_length=250):
         '''does split, lower, break into ddivs, tokenize, remove swords, and stem if desired'''
         
         if in_text is None:
@@ -62,9 +69,9 @@ class NLProcessor():
         if init_split_on=='database':
             if origdb is not None:
                 if '.' in break_on:
-                    p_output = process_text_sentences(in_text,origdb,swords)
+                    p_output = process_text_sentences(in_text,origdb,swords,ddiv_max_length,to_stem)
                 else:
-                    p_output = process_text_paragraphs(in_text,origdb,swords)
+                    p_output = process_text_paragraphs(in_text,origdb,swords,ddiv_max_length,to_stem)
                 atext = p_output[0]
                 self.removed_articles = p_output[1]
 
@@ -72,13 +79,123 @@ class NLProcessor():
         else:
             if '.' in break_on:
                 atext = [plist_to_slist([a]) for a in in_text]
-                atext = [[process_paragraph(s,swords) for s in art] for art in atext]
+                atext = [[process_paragraph(s,swords,to_stem) for s in art] for art in atext]
             else:
                 atext = [a.split(break_on) for a in in_text]
-                atext = [[process_paragraph(s,swords) for s in art] for art in atext]
+                atext = [[process_paragraph(s,swords,to_stem) for s in art] for art in atext]
 
         self.processed_text = atext
-        #return atext
+        self.word_count = sum([len(s) for s in atext])
+
+    def flatten_text(self,text='default'):
+        '''flattens text to include simply a list of all words in each corpus/article'''
+        if text=='default':
+            text = self.processed_text
+        self.flat_text = [flatten_list_of_lists(art) for art in text]
+
+    def get_word_count(self):
+        '''returns word count'''
+        return self.word_count
+
+    def get_removed_articles(self):
+        '''returns removed articles'''
+        return self.removed_articles
+
+
+class NLCorporizer(NLProcessor):
+    '''this is an NLProcessor that also creates and holds dictionaries, corpora, frequencies, etc'''
+
+    def __init__(self, raw_text=[], swords=None, corpus_native_path='corpus.mm'):
+        NLProcessor.__init__(self, raw_text, swords)
+        self.frequencies = None
+        self.dictionary = None
+        self.corpus_native = None
+        self.corpus_native_path = corpus_native_path
+
+    def count_frequencies(self,texts='default'):
+        '''counts all frequencies'''
+        if texts=='default':
+            if self.flat_text==None:
+                self.flatten_text()
+            texts = self.flat_text
+
+        self.frequencies = defaultdict(int)
+        for text in texts:
+            for token in text:
+                self.frequencies[token] += 1
+
+    def get_frequencies(self):
+        '''returns frequencies'''
+        return self.frequencies
+
+    def make_dictionary(self,texts='default',text_frequency_min=2):
+        '''creates dictionary'''
+        if self.frequencies==None:
+            self.count_frequencies(texts)
+
+        texts = [[token for token in text if frequency[token] > text_frequency_min]
+                for text in texts]
+        self.dictionary = corpora.Dictionary(texts)
+
+    def get_dictionary(self):
+        '''returns dictionary'''
+        return self.dictionary
+
+    def make_corpus(self,texts='default',fname='default'):
+        '''takes atext, a list of lists of words per article, and a dictionary, and returns/saves corpus'''
+        # check for dictionary 
+        if self.dictionary==None:
+            self.make_dictionary(texts)
+
+        # create corpus
+        self.corpus_native = [self.dictionary.doc2bow(art) for art in texts]
+
+        # store to disk, for later use
+        if fname=='default':
+            fname = corpus_native_path
+        else:
+            self.corpus_native_path = fname
+        corpora.MmCorpus.serialize(fname, corpus_native)
+
+    def get_corpus(self,fname='corpus.mm'):
+        '''loads and returns corpus'''
+        if self.corpus_native==None:
+            self.corpus_native = corpora.MmCorpus(fname)
+
+        return self.corpus_native
+
+
+class NLModeler(NLCorporizer):
+    '''This class hold models and transformations (tfidf, lsa, ...)'''
+
+    def __init__(self, raw_text=[], swords=None, corpus_native_path='corpus.mm'):
+        NLCorporizer.__init__(self, raw_text=[], swords=None, corpus_native_path='corpus.mm')
+        self.tfidf = None
+        self.corpus_tfidf = None
+        self.lsa = None
+        self.corpus_lsa = None
+
+    def tfidf(self,corpus='default'):
+        '''creates tfidf and model'''
+        if corpus=='default':
+            corpus = self.corpus_native
+
+        if corpus==None:
+            try:
+                self.get_corpus()
+            except:
+                self.make_corpus()
+            corpus = self.corpus_native
+
+        self.tfidf = models.TfidfModel(self.corpus_native)
+        self.corpus_tfidf = self.tfidf[self.corpus_native]
+
+
+
+
+
+
+######## HELPER FUNCTIONS FOR PROCESSING ########
         
 def process_paragraph(par,swords,to_stem=True):
     '''takes one paragraph (string); performs lower, tokenize, remove punctuation/stop words'''
@@ -86,39 +203,40 @@ def process_paragraph(par,swords,to_stem=True):
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(par)
     nstop_tokens = [t for t in tokens if (t not in swords and t not in string.punctuation)]
-    porter = PorterStemmer()
-    stokens = [porter.stem(t) for t in nstop_tokens]
-    return stokens
+    if to_stem:
+        porter = PorterStemmer()
+        nstop_tokens = [porter.stem(t) for t in nstop_tokens]
+    return nstop_tokens
 
-def process_text_paragraphs(atext,origdb,swords):
+def process_text_paragraphs(atext,origdb,swords,ddiv_max_length=250):
     '''atext is a list or series of textstrings (one from each article), 
     origdb is a list or series of corresponding original databse IDs (ints from 0-4)'''
     # initial text split
     alist = [initial_text_split(a,int(o)) for a,o in zip(atext,origdb)]
         
     # remove very long articles
-    removed_articles = [aix for aix,a in enumerate(alist) if len(a)>=250]
-    alist = [a for a in alist if len(a)<250]
+    removed_articles = [aix for aix,a in enumerate(alist) if len(a)>=ddiv_max_length]
+    alist = [a for a in alist if len(a)<ddiv_max_length]
     
     # process each paragraph
-    alist = [[process_paragraph(p,swords) for p in a] for a in alist]
+    alist = [[process_paragraph(p,swords,to_stem) for p in a] for a in alist]
 
     return [alist,removed_articles]
 
-def process_text_sentences(atext,origdb,swords):
+def process_text_sentences(atext,origdb,swords,ddiv_max_length=250):
     '''same processing, but does a sentence breakup rather than paragraph'''
     # initial text split
     alist = [initial_text_split(a,int(o)) for a,o in zip(atext,origdb)]
     
     # remove very long articles
-    removed_articles = [aix for aix,a in enumerate(alist) if len(a)>=250]
-    alist = [a for a in alist if len(a)<250]
+    removed_articles = [aix for aix,a in enumerate(alist) if len(a)>=ddiv_max_length]
+    alist = [a for a in alist if len(a)<ddiv_max_length]
     
     # change paragraph splits to sentence splits
     alist = [plist_to_slist(plist) for plist in alist]
     
     # process each paragraph
-    alist = [[process_paragraph(p,swords) for p in a] for a in alist]
+    alist = [[process_paragraph(p,swords,to_stem) for p in a] for a in alist]
         
     return [alist,removed_articles]
 
@@ -141,3 +259,7 @@ def plist_to_slist(plist):
     '''changes a list of paragraphs to a list of sentences'''
     spl = [re.split('[/./!/?]',par) for par in plist]
     return [s for p in spl for s in p if len(s)>1]
+
+def flatten_list_of_lists(lol):
+    l = []
+    return map(l.extend, lol)
